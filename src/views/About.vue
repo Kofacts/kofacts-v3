f1d6bea2648faf9866f79007c26576cfda4969de
Obodugo Rapheal <obodugorapheal@gmail.com>
2020-05-27 15:36:18 +0100

    fix issues

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about" style="color:white !important">
+    <h1>Privacy Policy for Movie Suggest</h1>
+    <p>
+      Effective Date: May 27, 2020
+
+      <br>
+
+      This Privacy Policy governs the information Movie Suggest collects on its websites and through its online services (the “Service”). The terms “Movie Suggest” “we,” “us,” and “our” include netflixparty.com and our affiliates and subsidiaries. This Privacy Policy explains how Movie Suggest may collect, use, and disclose information we obtain through the Service.
+      
+      
+      <br><br>
+      <h3>Information Collected on Movie Suggest</h3>
+      <br><br>
+Movie Suggest analyzes your viewing history on Netflix and uses that to suggest movies you would like based on history result. We don't store anything from that. All of the recommendation happens on the fly and ends there. We are looking to make the platform stable so it automatically keeps session of suggested movies, but that would be in the future.
+<br><br>
+<h3>Third party services </h3>
+<br><br>
+Movie Suggest  uses Chrome (Chromium) or WebExtensions Storage Sync API for storing user's settings and browser's Web Storage API (localStorage) for storing user's Developer Tools fixes.
+
+    </p>
   </div>
 </template>
