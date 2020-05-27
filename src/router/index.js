c351a8d4d6027c30a678fddcb7c1425c328328e7
Obodugo Rapheal <obodugorapheal@gmail.com>
2020-05-27 15:36:58 +0100

    fix issues

--- src/router/index.js
+++ src/router/index.js
@@ -11,17 +11,17 @@ const routes = [{
   },
   {
     path: '/movie-suggest/privacy-policy',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
